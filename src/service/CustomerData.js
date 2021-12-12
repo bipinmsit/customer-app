1cee1340d0af2418da740901379a2569b5d7ec2a
Bipin Kumar <bipin.kumar@laminaar.in>
2021-12-12 10:57:11 +0530

    storing data to local storage

--- src/service/CustomerData.js
+++ src/service/CustomerData.js
@@ -1,14 +1,26 @@
-
 let customers =[
     {id:1,name:'VIvek',email:'vivek@abc.com',phone:'89898989', address:'India'},
     {id:2,name:'Pari',email:'pari@abc.com',phone:'653423662', address:'Asia'},
     {id:3,name:'Rama',
     email:'rama@abc.com',phone:'653423662', address:'Asia', dob:new Date()}
   ];
+
+if(localStorage.getItem('_customers') != null){
+    try{
+       let temp = JSON.parse(localStorage.getItem('_customers'));
+       customers = temp;
+    }catch(e){
+    }
+}
+
+let saveLocalStorage = () =>{
+    localStorage.setItem('_customers',JSON.stringify(customers));
+}
 export const getCustomers = () =>(customers);
 
 export const addCustomer = (record) =>{
     customers.push(record);
+    saveLocalStorage();
 }
 export const updateCustomer = (record) =>{
     let tempList = customers.filter((item)=>(item.id==record.id));
@@ -18,9 +30,12 @@ export const updateCustomer = (record) =>{
         tempList[0].phone = record.phone;
         tempList[0].address = record.address;
     }
+    saveLocalStorage()
+
 }
 export const deleteCustomer = (id) =>{
     customers = customers.filter((item)=>(item.id!=id));
+    saveLocalStorage();
 }
 
 export const getCustomerById = (id) =>{
